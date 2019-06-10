cc844a2ef6419fd1b73b685abaa0d4a66a8ebf5a
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-06-10 16:47:10 -0500

    Correcciones de test, punto y comas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const paginaInicio = document.getElementById("paginaInicio")
-const paginaCodificar1  = document.getElementById("paginaCodificar1")
-const mensajeCodificado = document.getElementById("mensajeCodificado")
-const mensajeEnviado = document.getElementById("mensajeEnviado")
-const paginaDecodificar1  = document.getElementById("paginaDecodificar1")
-const mensajeDecodificado = document.getElementById("mensajeDecodificado")
-const vuelvePronto = document.getElementById("vuelvePronto")
+const paginaInicio = document.getElementById("paginaInicio");
+const paginaCodificar1  = document.getElementById("paginaCodificar1");
+const mensajeCodificado = document.getElementById("mensajeCodificado");
+const mensajeEnviado = document.getElementById("mensajeEnviado");
+const paginaDecodificar1  = document.getElementById("paginaDecodificar1");
+const mensajeDecodificado = document.getElementById("mensajeDecodificado");
+const vuelvePronto = document.getElementById("vuelvePronto");
 
-const iniciar = document.getElementById("iniciar")
-const buttonReturn = document.getElementById("buttonReturn")
-const buttonClose = document.getElementById("buttonClose")
-const buttonEnviar = document.getElementById ("buttonEnviar")
+const iniciar = document.getElementById("iniciar");
+const buttonReturn = document.getElementById("buttonReturn");
+//const buttonClose = document.getElementById("buttonClose");
+const buttonEnviar = document.getElementById ("buttonEnviar");
 
 //function cerrar()
 /*
@@ -25,54 +25,54 @@ const cerrar= () => {
 */
 //function irCodificar()
 const irCodificar = () => {
-		paginaInicio.style.display = "none"
-	  paginaCodificar1.style.display = "block"
-    buttonReturn.style.display = "block"
-  }
+		paginaInicio.style.display = "none";
+    paginaCodificar1.style.display = "block";
+    buttonReturn.style.display = "block";
+  };
 
 
 //function codificar()
 const codificar = () => {
-    paginaCodificar1.style.display = "none"
-    mensajeCodificado.style.display = "block"
-    buttonEnviar.style.display = "block"
-  }
+    paginaCodificar1.style.display = "none";
+    mensajeCodificado.style.display = "block";
+    buttonEnviar.style.display = "block";
+  };
 
 
 //function enviar()
 const enviar = () => {
-    mensajeCodificado.style.display = "none"
-    mensajeEnviado.style.display = "block"
-    iniciar.style.display = "block"
-    buttonEnviar.style.display = "none"
-  }
+    mensajeCodificado.style.display = "none";
+    mensajeEnviado.style.display = "block";
+    iniciar.style.display = "block";
+    buttonEnviar.style.display = "none";
+  };
 
 
 //function irDecodificar()
 const irDecodificar = () => {
-    paginaInicio.style.display = "none"
-    paginaDecodificar1.style.display = "block"
-    buttonReturn.style.display = "block"
-  }
+    paginaInicio.style.display = "none";
+    paginaDecodificar1.style.display = "block";
+    buttonReturn.style.display = "block";
+  };
 
 //function decodificar()  ç
 const decodificar = () => {
-    paginaDecodificar1.style.display = "none"
-    mensajeDecodificado.style.display = "block"
-    iniciar.style.display = "block"
-  }
+    paginaDecodificar1.style.display = "none";
+    mensajeDecodificado.style.display = "block";
+    iniciar.style.display = "block";
+  };
 
 
 //function irInicio()
 const irInicio = () => {
-    mensajeEnviado.style.display = "none"
-    mensajeDecodificado.style.display = "none"
-    vuelvePronto.style.display = "none"
-    iniciar.style.display = "none"
-    paginaInicio.style.display = "block"
+    mensajeEnviado.style.display = "none";
+    mensajeDecodificado.style.display = "none";
+    vuelvePronto.style.display = "none";
+    iniciar.style.display = "none";
+    paginaInicio.style.display = "block";
 
     document.getElementById("offsetCode").value= "";
     document.getElementById("offsetDecode").value= "";
     document.getElementById("message").value= "";
     document.getElementById("messageCifrado").value= "";
-  }
+  };
